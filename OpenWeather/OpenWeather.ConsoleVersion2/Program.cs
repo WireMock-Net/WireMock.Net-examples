7ffbd79f67a44011a71181ed6f3da6f0d37694a8
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-04-24 14:58:42 +0200

    add console v2

---
+++ OpenWeather/OpenWeather.ConsoleVersion2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using OpenWeather.Business;
+
+namespace OpenWeather.ConsoleVersion2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new OpenWeatherClient("https://wiremock.azurewebsites.net", "xxx");
+
+            var service = new OpenWeatherService(client);
+
+            var result = await service.GetInfoAsync("Sittard, NL");
+
+            System.Console.WriteLine($"{result.DegreesCelsius:##.##} celsius ({result.Description})");
+        }
+    }
+}
